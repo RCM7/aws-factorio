6d5ff47b0684ddb97e71e6e00902107bd34622c0
Ricardo Marques <ricardooo.marques@gmail.com>
2017-09-11 03:08:16 +0100

    Add deploy of api gateway
    Add usage plan with throttle and quota to api gateway deploy
    Add api gateway key

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -14,6 +14,7 @@ resource "aws_api_gateway_method" "post" {
   resource_id   = "${aws_api_gateway_resource.manage.id}"
   http_method   = "POST"
   authorization = "NONE"
+  api_key_required = true
 }
 
 resource "aws_api_gateway_integration" "lambda" {
@@ -45,4 +46,50 @@ resource "aws_api_gateway_integration_response" "lambda" {
   response_templates = {
     "application/json" = ""
   }
+}
+
+resource "aws_api_gateway_deployment" "factorio" {
+  depends_on = ["aws_api_gateway_method.post"]
+
+  rest_api_id = "${aws_api_gateway_rest_api.factorio.id}"
+  stage_name  = "factorio"
+
+  stage_description = "Live api for factorio management"
+
+}
+
+resource "aws_api_gateway_usage_plan" "usage_plan" {
+  name         = "rate-limiter"
+  description  = "Limit calls to 2/s and 1k a day"
+
+  api_stages {
+    api_id = "${aws_api_gateway_rest_api.factorio.id}"
+    stage  = "${aws_api_gateway_deployment.factorio.stage_name}"
+  }
+
+  quota_settings {
+    limit  = 1000
+    offset = 0
+    period = "DAY"
+  }
+
+  throttle_settings {
+    burst_limit = 1
+    rate_limit  = 1
+  }
+}
+
+resource "aws_api_gateway_api_key" "my_factorio_key" {
+  name = "my-factorio-key"
+
+  stage_key {
+    rest_api_id = "${aws_api_gateway_rest_api.factorio.id}"
+    stage_name  = "${aws_api_gateway_deployment.factorio.stage_name}"
+  }
+}
+
+resource "aws_api_gateway_usage_plan_key" "factorio" {
+  key_id        = "${aws_api_gateway_api_key.my_factorio_key.id}"
+  key_type      = "API_KEY"
+  usage_plan_id = "${aws_api_gateway_usage_plan.usage_plan.id}"
 }
